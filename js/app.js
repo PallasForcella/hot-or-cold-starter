829d13225540a6fa933c81e0619bf826a6934185
Pallas <pallasravae@yahoo.com>
2015-10-20 15:02:24 -0600

    clear feilds added to new game

--- js/app.js
+++ js/app.js
@@ -7,49 +7,64 @@ $(document).ready(function(){
 
   	});
 
-    var UserNumber;
-    var SecretNumber = 0;
-    var StartGame = false;
+//start a new game
+  	$("a.new").click(newGame);
+
+
+	$(".user-input").submit(function(event){
+		var userGuess= $("#userGuess").val();
+		userNumber = parseInt(userGuess, 10);
+		checkValid();
+		event.preventDefault();
+		$("ul#guessList").append("<li>" + userGuess + "</li>");
+		$(".user-input")[0].reset();
+		var count = document.getElementById('count');
+    	var number = count.innerHTML;
+    	number++;
+    	count.innerHTML = number;
+	});
+
+    var userNumber = null;
+    var secretNumber = 0;
+    var isStarted = false;
     
-    function CreateSecretNumber() {
-        SecretNumber = (Math.floor(Math.random()*100));
-        console.log("Secret number = " + SecretNumber);
+    function createSecretNumber() {
+        secretNumber = (Math.floor(Math.random()*100));
+        console.log("Secret number = " + secretNumber);
     }
 
-  	function NewGame(){
-	$("a.new").click(function(){
-  		StartGame=true;
-		CreateSecretNumber();
-		UserNumber = 0;
-	});
+  	function newGame(){
+		isStarted=true;
+		createSecretNumber();
+		userNumber = null;
+		count=0;
+		$("ul#guessList").empty();
+		$("#count").empty();
   	}
 
-	function EnterNumber(){
-		$(".button").click(function(){
-		UserNumber = parseInt(UserNumber, 10);
-	CheckValid();
-	});
-	}
-
-	function CheckValid(){
+	function checkValid(){
 	//checks if it's NaN
-	if ( isNaN(UserNumber)
-		)
-	{
-		alert("Please enter a valid number");
-	}
+		if ( isNaN(userNumber)){
+			alert("Please enter a valid number");
+			console.log(userNumber);
+		}
+	//check if between 1 and 100	
+		else if (userNumber>100){
+			alert("Please enter a number between 1 and 100");
+
+		}
 	}
 
 	// check number against secret number
-	function CheckNumber (){
-	if (UserNumber === SecretNumber){
+	function checkNumber (){
+	if (userNumber === secretNumber){
 		alert("You have won!");
 		}
-	else if (UserNumber <(SecretNumber-30) || UserNumber>(SecretNumber+30))
+	else if (userNumber <(secretNumber-30) || userNumber>(secretNumber+30))
 		{
 			alert("very cold");
 		}
-	else (UserNumber <(SecretNumber-20) || UserNumber>(SecretNumber+20))
+	else (userNumber <(secretNumber-20) || userNumber>(secretNumber+20))
 		{
 			alert("cold");
 		}
